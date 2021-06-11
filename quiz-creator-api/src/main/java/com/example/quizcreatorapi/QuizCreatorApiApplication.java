2861b3db6b7fc95a1abd03d84fb78a826d95fcec
ahsan memon <muhammadahsan52544@gmail.comgit config -lgit config --global user.email muhammadahsan52544@gmail.com>
2021-06-12 00:37:12 +0500

    Added Spring files

---
+++ quiz-creator-api/src/main/java/com/example/quizcreatorapi/QuizCreatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizcreatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizCreatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizCreatorApiApplication.class, args);
+    }
+
+}
---
+++ quiz-creator-api/src/test/java/com/example/quizcreatorapi/QuizCreatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizcreatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizCreatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
